db97f0dec1580c79c03cf64d294f4281df868df3
IvanPavlovets <ivan150287@gmail.com>
2024-01-24 17:43:09 +0700

    init commit

---
+++ src/main/java/ru/pavlovets/spring_ripper/SpringRipperApplication.java
@@ -0,0 +1,13 @@
+package ru.pavlovets.spring_ripper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRipperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRipperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/pavlovets/spring_ripper/SpringRipperApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pavlovets.spring_ripper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRipperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
